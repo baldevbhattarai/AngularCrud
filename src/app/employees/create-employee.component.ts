0c84c2b199f4ab7c0393713bf83d21166beddc91
Baldev <Baldev@DESKTOP-TGB9VTP>
2018-11-30 21:38:53 -0600

    passing data from parent to child component

--- src/app/employees/create-employee.component.ts
+++ src/app/employees/create-employee.component.ts
@@ -3,6 +3,8 @@ import {NgForm} from '@angular/forms';
 import { Department } from 'src/models/department.model';
 import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
 import { Employee } from 'src/models/employee.model';
+import { EmployeeService } from './employee.service';
+import { Router } from '@angular/router';
 
 
 
@@ -34,7 +36,8 @@ export class CreateEmployeeComponent implements OnInit {
   { id: 3, name: 'IT' },
   { id: 4, name: 'Payroll' }
 ];
-  constructor() { 
+  constructor(private _employeeService: EmployeeService,
+              private _router: Router) { 
     this.datePickerConfig = Object.assign({}, { 
       containerClass: 'theme-dark-blue', 
       showWeekNumbers: true,
@@ -45,8 +48,9 @@ export class CreateEmployeeComponent implements OnInit {
    
   }
 
-  saveEmployee(newEmployee: Employee): void {
-    console.log(newEmployee);
+  saveEmployee(): void {
+    this._employeeService.save(this.employee);
+    this._router.navigate(['list']);
   }
   togglePhotoPreview() {
     this.previewPhoto = !this.previewPhoto;
