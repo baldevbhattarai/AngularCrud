9b7ddafad3418adc4b40fb61f554ca9f6f354b2a
Baldev <Baldev@DESKTOP-TGB9VTP>
2018-12-02 16:07:30 -0600

    angular route loading indicator has been successfully implemented using spinner css class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
-
+import {
+  Router, NavigationStart, NavigationEnd,
+  NavigationCancel, NavigationError, Event
+} from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styles: ['../node_modules/bootstrap/dist/css/bootstrap.css']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'AngularCrud';
-  
+  showLoadingIndicator = true;
+
+  // Inject the Angular Router
+  constructor(private _router: Router) {
+    // Subscribe to the router events observable
+    this._router.events.subscribe((routerEvent: Event) => {
+
+      // On NavigationStart, set showLoadingIndicator to ture
+      if (routerEvent instanceof NavigationStart) {
+        this.showLoadingIndicator = true;
+      }
+
+      // On NavigationEnd or NavigationError or NavigationCancel
+      // set showLoadingIndicator to false
+      if (routerEvent instanceof NavigationEnd ||
+        routerEvent instanceof NavigationError ||
+        routerEvent instanceof NavigationCancel) {
+        this.showLoadingIndicator = false;
+      }
+
+    });
+  }
 }
